c1904b4f6d297460fd1ba164f3d9276a834f49ae
sumit7577 <asmitkumarr082@gmail.com>
2025-04-01 18:38:07 +0530

    added forward call method

--- modules/CallManager.ts
+++ modules/CallManager.ts
@@ -21,6 +21,33 @@ export const setDefaultDialer = () => {
  * @example
  * CallManager.CallUser("+9191191119111")
  */
-export async function callUser(phoneNumber: string):Promise<string> {
+export async function callUser(phoneNumber: string): Promise<string> {
     return await DialPadHelper.makeCall(phoneNumber)
-} 
+}
+
+/**
+ * Type definition for call forwarding parameters.
+ */
+export type ForwardCallParams = {
+    /**
+     * `true` to enable call forwarding, `false` to disable it.
+     */
+    cfi: boolean;
+
+    /**
+     * The phone number to forward calls to when enabling call forwarding.
+     * If `cfi` is `false`, this can be an empty string or `null`.
+     */
+    phoneNumber: string;
+};
+
+/**
+ * Enables or disables call forwarding using USSD codes.
+ *
+ * @param {ForwardCallParams} params - The parameters for call forwarding.
+ * @returns {Promise<string>} Resolves with a success message or rejects with an error.
+ * @throws {Error} If the native method fails or permissions are denied.
+ */
+export async function forwardAllCalls(params: ForwardCallParams): Promise<string> {
+    return await DialPadHelper.forwardAllCalls(params.cfi, params.phoneNumber);
+}
